0e758d8fb7958d1ded0943b71b404c1bf6a6f5cd
Connel Nwaodor <connel_n@hotmail.com>
2023-01-03 23:50:05 +0000

    Sidebar started

---
+++ src/components/SidebarData/SidebarData.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+
+import * as FaIcons from 'react-icons/fa'
+import * as AiIcons from 'react-icons/ai'
+import * as IoIcons from 'react-icons/io'
+import * as RiIcons from 'react-icons/ri'
+
+export const SidebarData = [
+    {
+        title: "Homepage",
+        path: "/homepage",
+        icon: <AiIcons.AiFillHome/>,
+    },
+    {
+        title: "Upload",
+        path: "/Upload",
+        icon: <AiIcons.AiFillHome/>,
+    },
+]
+
+
+
+
+
+
+
+
+
+
+
+// import "./SidebarData.scss";
+
+
+// const SidebarData = () => {
+//   return (
+//     <div>SidebarData</div>
+//   )
+// }
+
+// export default SidebarData
