55a3c467e7b6238f8ee4dc56ff16a956827bf7fe
Connel Nwaodor <connel_n@hotmail.com>
2023-01-05 15:29:10 +0000

    Added a route

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import './App.scss';
 import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import NewUserForm from './components/NewUserForm/NewUserForm';
-import RatingDropdown from './components/RatingDropdown/RatingDropdown';
 import SearchBox from './components/SearchBox/SearchBox';
 import SignInForm from './components/SignInForm/SignInForm';
 import LandingPage from './containers/LandingPage/LandingPage';
 import Nav from './containers/Nav/Nav';
+import NewProperty from './containers/NewProperty/NewProperty';
 
 function App() {
 
@@ -16,18 +16,7 @@ function App() {
   //   image:""
   // });
 
-  const options = [
-    {value: 1, label: "1"},
-    {value: 2, label: "2"},
-    {value: 3, label: "3"},
-    {value: 4, label: "4"},
-    {value: 5, label: "5"},
-    {value: 6, label: "6"},
-    {value: 7, label: "7"},
-    {value: 8, label: "8"},
-    {value: 9, label: "9"},
-    {value: 10, label: "10"}
-  ];
+
 
   return (
     <Router>
@@ -35,13 +24,13 @@ function App() {
         <Nav/>
         <Routes>
           <Route>
-            {/* <RatingDropdown onChange={(value) => console.log(value)} placeHolder="Select..." options={options}/> */}
+            
           </Route>
           <Route path='#' 
             element={"#"}>
           </Route>
-          <Route path='#' 
-            element={"#"}>
+          <Route path='/upload' 
+            element={<NewProperty/>}>
           </Route>
           <Route path='#' 
             element={"#"}>
--- src/containers/NewProperty/NewProperty.jsx
+++ src/containers/NewProperty/NewProperty.jsx
@@ -1,14 +1,30 @@
 import "./NewProperty.scss";
+import RatingDropdown from '../../components/RatingDropdown/RatingDropdown';
 
-
-
-const NewProperty = () => {
 
     //a page where a new property location can be added
     // a form that gathers all the necessary  information and sends it to firebase
 
+const NewProperty = () => {
+
+  const options = [
+    {value: 1, label: "1"},
+    {value: 2, label: "2"},
+    {value: 3, label: "3"},
+    {value: 4, label: "4"},
+    {value: 5, label: "5"},
+    {value: 6, label: "6"},
+    {value: 7, label: "7"},
+    {value: 8, label: "8"},
+    {value: 9, label: "9"},
+    {value: 10, label: "10"}
+  ];
+
   return (
-    <div>NewProperty</div>
+    <div>
+      <input type="file" accept="image/*" name="" id="" />
+      <RatingDropdown onChange={(value) => console.log(value)} placeHolder="Select..." options={options}/>
+    </div>
   )
 }
 
