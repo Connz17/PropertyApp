c22c3a3f7f715f501c8d1b70a10cbc6c038311fa
Connel Nwaodor <connel_n@hotmail.com>
2023-01-10 16:26:24 +0000

    Property form complete

--- src/components/UploadScreen/UploadScreen.jsx
+++ src/components/UploadScreen/UploadScreen.jsx
@@ -12,15 +12,57 @@ const UploadScreen = ({property, setProperty, toggleUpload}) => {
         newProperty.images.main = URL.createObjectURL(e.target.files[0])
         setProperty(newProperty);
       }
+
+      const imageSelectDining = (e) =>{
+        const newProperty = {...property}; 
+        newProperty.images.dining = URL.createObjectURL(e.target.files[0])
+        setProperty(newProperty);
+      }
+
+      const imageSelectKitchen = (e) =>{
+        const newProperty = {...property}; 
+        newProperty.images.kitchen = URL.createObjectURL(e.target.files[0])
+        setProperty(newProperty);
+      }
+
+      const imageSelectBathroom = (e) =>{
+        const newProperty = {...property}; 
+        newProperty.images.bathroom = URL.createObjectURL(e.target.files[0])
+        setProperty(newProperty);
+      }
+
+      const imageSelectGarden = (e) =>{
+        const newProperty = {...property}; 
+        newProperty.images.garden = URL.createObjectURL(e.target.files[0])
+        setProperty(newProperty);
+      }
     console.log(property.images);
     
       console.log(property);
     
-      const bedroomSelect = (e) =>{
+      const masterBedroomSelect = (e) =>{
         const newProperty = {...property}; 
-        newProperty.images.bedrooms = URL.createObjectURL(e.target.files[0])
+        newProperty.images.bedrooms.master = URL.createObjectURL(e.target.files[0])
         setProperty(newProperty)
       }
+      const secondBedroomSelect = (e) =>{
+        const newProperty = {...property}; 
+        newProperty.images.bedrooms.second = URL.createObjectURL(e.target.files[0])
+        setProperty(newProperty)
+      }
+
+      const thirdBedroomSelect = (e) =>{
+        const newProperty = {...property}; 
+        newProperty.images.bedrooms.third = URL.createObjectURL(e.target.files[0])
+        setProperty(newProperty)
+      }
+
+      const fourthBedroomSelect = (e) =>{
+        const newProperty = {...property}; 
+        newProperty.images.bedrooms.fourth = URL.createObjectURL(e.target.files[0])
+        setProperty(newProperty)
+      }
+
 
 
 
@@ -30,12 +72,16 @@ const UploadScreen = ({property, setProperty, toggleUpload}) => {
     <h4 className="upload-window__label">Upload property pictures</h4>
     <FaIcons.FaWindowClose className="upload-window__close" onClick={toggleUpload}/>
       <div className="upload-window__form">
-        <ImageSelect roomTitle={"Main room"} handleImageSelection={imageSelectMain} roomImage={property.images.main}/> 
-        {/* <input type="file" accept="image/*" name="file-select" id="" onChange={imageSelect}/>
-        <input type="file" accept="image/*" name="file-select" id="" onChange={bedroomSelect}/>
-        <div className="preview__container">
-            <img src={property.images.kitchen} alt="preview" />
-        </div>         */}
+        <ImageSelect roomTitle={"Main room"} handleImageSelection={imageSelectMain} roomImage={property.images.main}/> <br />
+        <ImageSelect roomTitle={"Dining Room"} handleImageSelection={imageSelectDining} roomImage={property.images.dining}/> <br />
+        <ImageSelect roomTitle={"Kitchen"} handleImageSelection={imageSelectKitchen} roomImage={property.images.kitchen}/> <br />
+        <ImageSelect roomTitle={"Bathroom"} handleImageSelection={imageSelectBathroom} roomImage={property.images.bathroom}/> <br />
+        <ImageSelect roomTitle={"Garden"} handleImageSelection={imageSelectGarden} roomImage={property.images.garden}/> <br />
+        <h5 className="upload-window__label">Upload bedroom pictures</h5>
+        <ImageSelect roomTitle={"Master bedroom"} handleImageSelection={masterBedroomSelect} roomImage={property.images.bedrooms.master}/><br />
+        <ImageSelect roomTitle={"Second bedroom"} handleImageSelection={secondBedroomSelect} roomImage={property.images.bedrooms.second}/><br />
+        { property.images.bedrooms.master && <ImageSelect roomTitle={"Third bedroom"} handleImageSelection={thirdBedroomSelect} roomImage={property.images.bedrooms.third}/>} <br />
+        { property.images.bedrooms.second && <ImageSelect roomTitle={"Fourth bedroom"} handleImageSelection={fourthBedroomSelect} roomImage={property.images.bedrooms.fourth}/> }
       </div>
     </div>
   )
